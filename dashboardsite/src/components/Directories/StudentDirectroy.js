c47a116508d3272b8739576d6edeb04f5bf5ebc7
newgazar1 <72676296+newgazar1@users.noreply.github.com>
2022-05-26 11:58:23 -0400

    integrated databases for directories
    Firebase integration of database with directory tables

--- dashboardsite/src/components/Directories/StudentDirectroy.js
+++ dashboardsite/src/components/Directories/StudentDirectroy.js
@@ -1,67 +1,78 @@
-import React from 'react' 
-import {TableContainer, Table,TableHead,TableBody,TableRow,TableCell,Paper} from '@mui/material'
+import React from 'react';
+import {useState, useEffect} from 'react';
+import {TableContainer, Table,TableHead,TableBody,TableRow,TableCell,Paper} from '@mui/material';
 
-import {collection, getDocs} from 'firebase/firestore'
-import db from './firebase'
-
-
+import {collection, getDocs} from 'firebase/firestore';
+import db from './firebase';
 
 const StudentDirectroy = () => {
+    const [students, setStudents] = useState([]); 
+    useEffect(() => {
+        const studentList = []
+        getDocs(collection(db, "students"))
+        .then((allStudents) => {
+            allStudents.forEach((student) => 
+            studentList.push({firstName:student.firstName, lastName:student.lastName, email:student.email, birthday:student.birthday, id:student.studentId,  ...student.data()
+            }))
+            return studentList
+        }).then(
+            studentList=>setStudents(studentList)
+        )
+  }, [])
 
-    getDocs(collection(db, "students")).then((allDocs) => {allDocs.forEach((doc) => console.log(doc.data()))})
-
-
-
-    const studentList = [
-        {
-            firstName: "Jack",
-            lastName: "Student1",
-            year: "4th Year"
-         },
-        {
-            firstName: "Jack",
-            lastName: "Student2",
-            year: "3rd Year"
-        },
-        {
-            firstName: "Jack",
-            lastName: "Student3",
-            year: "2nd Year"
-        },
-        {
-
-            firstName: "Jack",
-            lastName: "Student4",
-            year: "1st Year"
-        }
-    ];
-
-
-    return <TableContainer component={Paper}>
+    return (
+        
+    <TableContainer component={Paper}>
         <Table aria-label='StudentDirectory'>
             <TableHead>
                 <TableRow>
                     <TableCell align='center'>First Name</TableCell>
                     <TableCell align='center'>Last Name</TableCell>
-                    <TableCell align='center'>Year</TableCell>
+                    <TableCell align='center'>Email</TableCell>
                 </TableRow>
             </TableHead>
             <TableBody>
                 {
-                    studentList.map(row => (
+                    students.length>0
+                    &&
+                    students.map(row => (
                         <TableRow key={row.email}
                         sx={{'&:last-child td, &:last-childth':{border: 0}}}>
                             <TableCell align='center'>{row.firstName}</TableCell>
                             <TableCell align='center'>{row.lastName}</TableCell>
-                            <TableCell align='center'>{row.year}</TableCell>
+                            <TableCell align='center'>{row.email}</TableCell>
                         </TableRow>
                     ))
                 }
             </TableBody>
         </Table>
-    </TableContainer>
+    </TableContainer>)
             
     
 }
 
 export default StudentDirectroy
+
+// const studentList1 = [
+    //     {
+    //         firstName: "Jack",
+    //         lastName: "Student1",
+    //         email: "1"
+    //      },
+    //     {
+    //         firstName: "Jack",
+    //         lastName: "Student2",
+    //         email: "2"
+    //     },
+    //     {
+    //         firstName: "Jack",
+    //         lastName: "Student3",
+    //         email: "3"
+    //     },
+    //     {
+
+    //         firstName: "Jack",
+    //         lastName: "Student4",
+    //         email: "4"
+    //     }
+    // ];
--- dashboardsite/src/components/Directories/TeacherDirectory.js
+++ dashboardsite/src/components/Directories/TeacherDirectory.js
@@ -1,70 +1,92 @@
-import React from 'react' 
-import {TableContainer, Table,TableHead,TableBody,TableRow,TableCell,Paper} from '@mui/material'
+import React from 'react';
+import {useState, useEffect} from 'react';
+import {TableContainer, Table,TableHead,TableBody,TableRow,TableCell,Paper} from '@mui/material';
+
+import {collection, getDocs} from 'firebase/firestore';
+import db from './firebase';
 
 const TeacherDirectory = () => {
+    const [teachers, setTeachers] = useState([]); 
+    useEffect(() => {
+        const teacherList = []
+        getDocs(collection(db, "teachers"))
+        .then((allTeachers) => {
+            allTeachers.forEach((teacher) => 
+            teacherList.push({firstName:teacher.firstName, lastName:teacher.lastName, email:teacher.email, password:teacher.password, id:teacher.id, grade:teacher.grade,  ...teacher.data()
+            }))
+            return teacherList
+        }).then(
+            teacherList=>setTeachers(teacherList)
+        )
+  }, [])
 
-   
-
-
-    const teachersList = [
-        {
-            email: 1,
-            password: 123,
-            firstName: "John",
-            lastName: "Doe1",
-            subject: "math"
-         },
-        {
-            email: 2,
-            password: 123,
-            firstName: "John",
-            lastName: "Doe2",
-            subject: "histroy"
-        },
-        {
-            email: 3,
-            password: 123,
-            firstName: "John",
-            lastName: "Doe3",
-            subject: "science"
-        },
-        {
-            email: 4,
-            password: 123,
-            firstName: "John",
-            lastName: "Doe4",
-            subject: "english"
-        }
-    ];
-
-
-    return <TableContainer component={Paper}>
+    return (
+    
+    <TableContainer component={Paper}>
         <Table aria-label='Teacher Directory'>
             <TableHead>
                 <TableRow>
                     <TableCell>First Name</TableCell>
                     <TableCell>Last Name</TableCell>
-                    <TableCell>Subject</TableCell>
+                    <TableCell>Grade</TableCell>
                     <TableCell>Email</TableCell>
                 </TableRow>
             </TableHead>
             <TableBody>
                 {
-                    teachersList.map(row => (
+                    teachers.length>0
+                    &&
+                    teachers.map(row => (
                         <TableRow key={row.email}
                         sx={{'&:last-child td, &:last-childth':{border: 0}}}>
                             <TableCell>{row.firstName}</TableCell>
                             <TableCell>{row.lastName}</TableCell>
-                            <TableCell>{row.subject}</TableCell>
+                            <TableCell>{row.grade}</TableCell>
                             <TableCell>{row.email}</TableCell>
                         </TableRow>
                     ))
                 }
             </TableBody>
         </Table>
-    </TableContainer>
+    </TableContainer>)
             
     
 }
 
 export default TeacherDirectory
+
+
+    //const teachersList = [
+    //     {
+    //         email: 1,
+    //         password: 123,
+    //         firstName: "John",
+    //         lastName: "Doe1",
+    //         grade: "1",
+    //         email: "1"
+    //      },
+    //     {
+    //         email: 2,
+    //         password: 123,
+    //         firstName: "John",
+    //         lastName: "Doe2",
+    //         grade: "2",
+    //         email: "2"
+    //     },
+    //     {
+    //         email: 3,
+    //         password: 123,
+    //         firstName: "John",
+    //         lastName: "Doe3",
+    //         grade: "3",
+    //         email: "3"
+    //     },
+    //     {
+    //         email: 4,
+    //         password: 123,
+    //         firstName: "John",
+    //         lastName: "Doe4",
+    //         grade: "4",
+    //         email: "4"
+    //     }
+    // ];
