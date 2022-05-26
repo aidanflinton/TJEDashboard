20ecd125d6077def3dc151b33d984dbb69ef1a32
Verified✔️ Zelaya <qft2jk@virginia.edu>
2022-05-26 13:56:20 -0400

    classpages

--- dashboardsite/src/App.js
+++ dashboardsite/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Sidebar from "./Componets/SideBar/SideBar.js"
+import Card from "./Componets/ClassPage/Card.js"
+import "./Componets/ClassPage/Card.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Wrapper">
+    <div className="SideBars">
+
+    <Sidebar/>
+
     </div>
-  );
+    <Card
+    img="https://media.istockphoto.com/photos/female-teacher-pointing-with-finger-at-mathematical-equation-on-in-picture-id1080232656?k=20&m=1080232656&s=612x612&w=0&h=7OszmnpcTXIiIhqUXUL3sOaI-nn9DisJU8z3ceeHL5k="
+    title="Jane Doe"
+    description="This course is about ....."
+    contact= "janedoe@virginia.edu"
+    grade="A"
+    />
+     <Card
+    img="https://media.istockphoto.com/photos/back-to-school-picture-id485083346?k=20&m=485083346&s=612x612&w=0&h=U0URwGcXqdIK8IplP5en1w4_HtLadzqQbvEO3I5JLkY="
+    title="Jill Doe"
+    description="This course is about ....."
+    contact= "jilldoe@virginia.edu"
+    grade= "B+"
+    />
+     <Card
+    img="https://media.istockphoto.com/photos/businessman-picture-id502912865?k=20&m=502912865&s=612x612&w=0&h=UJ_IQFCRMLuYz9vsA1VQyc99nit0nkPrEPqhAm_obno="
+    title="John Doe"
+    description="This course is about ....."
+    contact= "johndoe@virginia.edu"
+    grade="A-"
+    />
+  </div>
+  )
+;
+    
 }
 
 export default App;
---
+++ dashboardsite/src/Componets/SideBar/ClassData.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import HomeIcon from '@mui/icons-material/Home';
+import LooksOneIcon from '@mui/icons-material/LooksOne';
+import LooksTwoIcon from '@mui/icons-material/LooksTwo';
+import Looks3Icon from '@mui/icons-material/Looks3';
+import Looks4Icon from '@mui/icons-material/Looks4';
+import Looks5Icon from '@mui/icons-material/Looks5';
+
+export const ClassData=[
+    {
+
+        title:"Home",
+        icon:<HomeIcon/>,
+        link:"/home"
+    },
+    {
+
+        title:"Kindergarden",
+        icon:<HomeIcon/>,
+        link:"/Kindergarden"
+    },
+    {
+
+        title:"1st Grade",
+        icon:<LooksOneIcon/>,
+        link:"/1stgrade"
+    },
+    {
+
+        title:"2nd Grade",
+        icon:<LooksTwoIcon/>,
+        link:"/2ndgrade"
+    },
+    {
+
+        title:"3rd Grade",
+        icon:<Looks3Icon/>,
+        link:"/3rdgrade"
+    },
+    {
+
+        title:"4th Grade",
+        icon:<Looks4Icon/>,
+        link:"/4thgrade"
+    },
+    {
+
+        title:"5th Grade",
+        icon:<Looks5Icon/>,
+        link:"/5thgrade"
+    }
+]
